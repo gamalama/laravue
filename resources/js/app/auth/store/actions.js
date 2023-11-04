94c6c6c938b74af830b8f65ff4833ce03d912a43
Fakhry <fakhry.linux@gmail.com>
2023-11-05 01:33:41 +0800

    Mengakses API Endpoint User dan Menyimpan Datanya pada State

--- resources/js/app/auth/store/actions.js
+++ resources/js/app/auth/store/actions.js
@@ -1,4 +1,5 @@
 import {setHttpToken} from "../../../helpers";
+import {isEmpty} from "lodash";
 
 export const register = ({dispatch}, {payload, context}) => {
     return axios
@@ -24,13 +25,39 @@ export const login = ({dispatch}, {payload, context}) => {
         });
 }
 
-export const setToken = ({commit}, token) => {
+export const setToken = ({commit, dispatch}, token) => {
+    if (isEmpty(token)) {
+        return dispatch("checkTokenExists").then(token => {
+            setHttpToken(token);
+        });
+    }
+
     commit("setToken", token);
     setHttpToken(token);
 }
 
+export const removeToken = ({commit}) => {
+    commit("setAuthenticated", false);
+    commit("setUserData", null);
+    commit("setToken", null);
+    setHttpToken(null);
+}
 
-export const fetchUser = ({commit}, user) => {
-    commit("setAuthenticated", true);
-    commit("setUserData", user);
+export const fetchUser = ({commit}) => {
+    axios.get("/api/user").then(result => {
+        commit("setAuthenticated", true);
+        commit("setUserData", result.data.data);
+    }).catch(err => {
+        console.log(err.response.data);
+    });
+}
+
+export const checkTokenExists = () => {
+    const token = localStorage.getItem('access_token');
+
+    if (isEmpty(token)) {
+        return Promise.reject("NO_STORAGE_FOUND");
+    }
+
+    return Promise.resolve(token);
 }
